e52df65011ab347a0c4bc125d30ada5eb90206f4
Swarnendu <swarnendumaity633@gmail.com>
2024-04-29 18:30:25 +0530

    Compiler Btn Design and adding Jdoole

---
+++ components/CompilerBtn.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material";
+import React from 'react'
+
+const CompilerBtn = () => {
+  return (
+   <section>
+    <Button variant= "contained" color="primary"  >
+     
+    </Button>
+   </section>
+  )
+}
+
+export default CompilerBtn
+
