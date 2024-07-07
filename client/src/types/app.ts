66ea2bd0955c4627d5632c4dc325449d8d7f8d65
Swarnendu <swarnendumaity633@gmail.com>
2024-07-07 11:42:08 +0530

    Adding all the types

--- client/src/types/app.ts
+++ client/src/types/app.ts
@@ -1,24 +1,27 @@
-import { RemoteUser, User, USER_STATUS } from "./user";
-import { StoreSnapshot, TLRecord } from "@tldraw/tldraw";  
+import { StoreSnapshot, TLRecord } from "@tldraw/tldraw"
+import { RemoteUser, User, USER_STATUS } from "./user"
 
-export type DrawingData = StoreSnapshot<TLRecord> | null;
+type DrawingData = StoreSnapshot<TLRecord> | null
 
-export interface AppContextType {
-    users: RemoteUser[];
-    setUsers: (
-        users: RemoteUser[] | ((users: RemoteUser[]) => RemoteUser[])
-    ) => void;
-    currentUser: User;
-    setCurrentUser: (user: User) => void;
-    status: USER_STATUS;
-    setStatus: (status: USER_STATUS) => void;
-    activityState: ACTIVITY_STATE;
-    setActivityState: (activityState: ACTIVITY_STATE) => void;
-    drawingData: DrawingData;
-    setDrawingData: (data: DrawingData) => void;
-}
-
-export enum ACTIVITY_STATE {
+enum ACTIVITY_STATE {
     CODING = "coding",
     DRAWING = "drawing",
 }
+
+interface AppContext {
+    users: RemoteUser[]
+    setUsers: (
+        users: RemoteUser[] | ((users: RemoteUser[]) => RemoteUser[]),
+    ) => void
+    currentUser: User
+    setCurrentUser: (user: User) => void
+    status: USER_STATUS
+    setStatus: (status: USER_STATUS) => void
+    activityState: ACTIVITY_STATE
+    setActivityState: (state: ACTIVITY_STATE) => void
+    drawingData: DrawingData
+    setDrawingData: (data: DrawingData) => void
+}
+
+export { ACTIVITY_STATE }
+export { AppContext, DrawingData }
--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -1,26 +1,28 @@
-export interface User {
+enum USER_CONNECTION_STATUS {
+    OFFLINE = "offline",
+    ONLINE = "online",
+}
+
+interface User {
     username: string
     roomId: string
 }
 
-export enum USER_CONNECTION_STATUS {
-    OFFLINE = "offline",
-    ONLINE= "online"
-}
-
-export interface RemoteUser extends User{
-    status: USER_CONNECTION_STATUS,
-    cursorPosition: number,
-    typing: boolean,
-    currentFile: string,
+interface RemoteUser extends User {
+    status: USER_CONNECTION_STATUS
+    cursorPosition: number
+    typing: boolean
+    currentFile: string
     socketId: string
 }
- 
- export enum USER_STATUS {
-        INITIAL = "initial",
-        CONNECTING = "connecting",
-        ATTEMPTING_JOIN = "attempting-join",
-        JOINED = "joined",
-        CONNECTION_FAILED = "connection-failed",
-        DISCONNECTED = "disconnected",
-    }
+
+enum USER_STATUS {
+    INITIAL = "initial",
+    CONNECTING = "connecting",
+    ATTEMPTING_JOIN = "attempting-join",
+    JOINED = "joined",
+    CONNECTION_FAILED = "connection-failed",
+    DISCONNECTED = "disconnected",
+}
+
+export { USER_CONNECTION_STATUS, USER_STATUS, RemoteUser, User }
