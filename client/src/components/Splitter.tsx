3cd93eaab8efcdb7b618f7b1d395d16b261e15e0
Swarnendu <swarnendumaity633@gmail.com>
2024-07-13 00:01:14 +0530

    Splitter JS

--- client/src/components/Splitter.tsx
+++ client/src/components/Splitter.tsx
@@ -1,8 +1,15 @@
+import  { useViews } from "@/context/ViewContext"
+import useLocalStorage from "@/hooks/useLocalStorage"
+import useWindowDimensions from "@/hooks/useWindowDimensions"
 import { ReactNode } from "react"
 import Split from "react-split"
- 
+  
 const Splitter = ({children}:{children: ReactNode}) => {
 
+  //import 3 and build custom hooks => useViews, useWindowDimensions, useLocalStorage
+   const {isMobile,width} = useWindowDimensions()
+   const {getItem, setItem}  = useLocalStorage();
+   const { isSidebarOpen} = useViews()
   const getGutter = ()=>{
     const gutter = document.createElement("div")
     gutter.className = "h-full cursor-e-resizer hidden md:block ";
@@ -10,20 +17,48 @@ const Splitter = ({children}:{children: ReactNode}) => {
     return gutter;
   }
 
+// If isMobile remove the first part
+  //Get the item in localStorage with string "editorSizes"
+  //set sizes
+  //if savedSizes then sizes is saves as JSON
+  //return sizes if isSidebarOpen else return [0,width]
   const getSizes = ()=>{
-     
+     if(isMobile) return [0,width]
+     const savedSizes = getItem('editorsizes')
+     let sizes = [35,65];
+     if(savedSizes){
+      sizes = JSON.parse(savedSizes)
+     } 
+     return isSidebarOpen ? sizes : [0,width]
   }
+
+  const getMaxSizes = ()=>{
+    if(isMobile) return [0, width]
+    return isSidebarOpen ? [Infinity, Infinity] : [0, Infinity]
+  }
+
+  const getMinSizes = ()=>{
+    if(isMobile) return [0, width]
+    return isSidebarOpen ? [350,350] : [50, 0]
+  }
+
+  const handleDrag = (sizes: number[])=>{
+     setItem("editorsizes", JSON.stringify(sizes))
+
+  }
+  
   return (
      <Split
-     sizes={[25, 75]}
-     minSize={100}
-     expandToMin={false}
+     sizes={getSizes()}
+     minSize={getMinSizes()}
+     maxSize={getMaxSizes()}
      gutter={getGutter}
      gutterAlign="center"
      snapOffset={30}
      dragInterval={1}
      direction="horizontal"
      cursor="e-resize"
+     onDrag={handleDrag}
      >
       {children}
      </Split>
--- client/src/context/ViewContext.tsx
+++ client/src/context/ViewContext.tsx
@@ -1,10 +1,19 @@
 // src/context/ViewContext.tsx
 
-import React, { createContext, ReactNode, useState } from 'react';
+import  { createContext, ReactNode, useContext, useState } from 'react';
 import { ViewContextType } from '@/types/view';
 
 // Create the context
 const ViewContext = createContext<ViewContextType | null>(null);
+
+export const useViews = (): ViewContextType => {
+    const context = useContext(ViewContext)
+    if (!context) {
+         
+        throw new Error("useViews must be used within a ViewContextProvider")
+    }
+    return context
+}
 
 const ViewContextProvider = ({ children }: { children: ReactNode }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
@@ -16,4 +25,5 @@ const ViewContextProvider = ({ children }: { children: ReactNode }) => {
     );
 };
 
-export { ViewContext, ViewContextProvider };
+export default ViewContext
+export {ViewContextProvider };
