b707523bedd9b259918c9315fd74742334c1d35c
Swarnendu <swarnendumaity633@gmail.com>
2024-07-26 18:52:26 +0530

    Some Bug fixes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,9 +5,9 @@ import AppProvider from "./context/AppProvider.tsx"
 import "@/styles/global.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    // <React.StrictMode>
+  
     <AppProvider>
         <App />
     </AppProvider>,
-    // </React.StrictMode>
+   
 )
